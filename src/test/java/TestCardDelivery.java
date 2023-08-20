a95ed1a8d35e0c03a54ff10c771c5123fa65707e
VladimirEnot <105981763+VladimirEnot@users.noreply.github.com>
2023-08-20 19:11:13 +0300

    add new test+new build.gradle

--- src/test/java/TestCardDelivery.java
+++ src/test/java/TestCardDelivery.java
@@ -1,5 +1,4 @@
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
@@ -7,27 +6,28 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.exist;
+import static com.codeborne.selenide.Selenide.*;
 
 public class TestCardDelivery {
-    private String genDate(int addDays, String pattern){
-        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+
+
+    String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
+
     @Test
-    void testFormRegistration(){
-        Configuration.holdBrowserOpen = true;
-        open("http://localhost:9999/");
-        $("[data-test-id = city] input").setValue("Петрозаводск");
-        String testData = genDate(4, "dd.MM.yyyy");
-        $("[data-test-id = date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
-        $("[data-test-id = date] input").sendKeys(testData);
-        $("[data-test-id = name] input").setValue("Николай Петрович");
-        $("[data-test-id = phone] input").setValue("+79111111111");
-        $("[data-test-id = agreement]").click();
-        $(".button").click();
-        $(".notification__title").shouldBe(visible, Duration.ofSeconds(15));
-        $(".notification__content").shouldHave(Condition.exactText("Встреча успешно забронирована на " + testData));
+    void shouldGetDeliveryCard() {
+        String date = generateDate(4);
+        Selenide.open("http://localhost:9999");
+        $("[data-test-id = 'city'] input").setValue("Петрозаводск");
+        $("[data-test-id = 'date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id = 'date'] input").setValue(date);
+        $("[data-test-id= 'name'] input").setValue("Никитин Александр");
+        $("[data-test-id = 'phone'] input").setValue("+79007778990");
+        $("[data-test-id = 'agreement']").click();
+        $$("[type = button]").find(exactText("Забронировать")).click();
+        $("[data-test-id = 'notification']").shouldHave(exactText("Успешно! Встреча успешно забронирована на " + date), Duration.ofMillis(15000)).shouldBe(exist);
     }
 }
